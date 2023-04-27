b7a4fcb52fa398d6610cac807f1d33c9746371bc
Aluno talentos <rafael.rodrigues022@hotmail.com>
2023-04-26 21:30:10 -0300

    exerciciosoperadores

---
+++ index.js
@@ -0,0 +1,53 @@
+/*const bool1 = true
+const bool2 = false
+const bool3 = !bool2 // bool3 é true
+
+let resultado = bool1 && bool2
+console.log("a. ", resultado) // resultado é false
+
+resultado = bool1 && bool2 && bool3
+console.log("b. ", resultado) // false
+
+resultado = !resultado && (bool1 || bool2)
+console.log("c. ", resultado) // true
+
+console.log("d. ", typeof resultado) // boolean
+///
+let primeiroNumero = prompt("Digite um numero!")
+let segundoNumero = prompt("Digite outro numero!")
+const soma = primeiroNumero + segundoNumero
+console.log(soma) // será impresso os números em string, não serão somados os números 
+///
+
+let primeiroNumero1 = Number(prompt("Digite um número!"))
+let segundoNumero2= Number(prompt("Digite outro número!"))
+const soma2 = primeiroNumero1 + segundoNumero2
+console.log(soma2)
+///
+
+const nome = Number(prompt("Qual é a sua idade?"));
+const nomeAmigo = Number(prompt("Qual é a idade do seu amigo?"));
+console.log("Sua idade é maior do que a do seu melhor amigo?", nome > nomeAmigo);
+console.log("A diferença das idades é", nome-nomeAmigo);
+///
+
+const par = Number(prompt("digite um número par"));
+console.log(par%2); // todos os números pares vão dar 0
+///
+
+const idade = Number(prompt("Quantos anos você tem?"));
+console.log(idade * 12, "meses");
+console.log(idade * 365, "dias");
+console.log(idade * 8766, "horas");
+///
+
+const n1 = Number(prompt("digite um número"));
+const n2 = Number(prompt("digite outro número"));
+console.log("O primeiro numero é maior que o segundo?"", n1>n2)
+console.log("O primeiro numero é igual ao segundo?", n1==n2)
+console.log("O primeiro numero é divisivel pelo segundo?", n1%n2 == 0)
+console.log("O segundo numero é divisivel pelo primeiro?", n2%n1 ==0)*/
+///
+
+
+
